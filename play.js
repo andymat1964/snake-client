5210ccdc2adcdfb90eeed514f887ca8d27a5c33c
Andrew Matthews <andymat1964@gmail.com>
2020-12-02 21:08:47 +0000

    added index.js

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
+const { setupInput } = require('./input');
+
 // play.js
 const { connect } = require('./client');
 console.log('Connecting ...');
-connect()
+connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  };
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
-setupInput(); 
+setupInput();
